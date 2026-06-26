b5787e57b16bf746ea8e5926c01f663ff6c7e4ab
Mark <84234570+Mark-a-obrien@users.noreply.github.com>
2026-06-26 21:21:31 +0100

    Update
    Added fastapi

--- app.py
+++ app.py
@@ -1,40 +1,8 @@
-from db import *
+from fastapi import FastAPI
 
-def createNote():
-    title = input("Enter title: ")
-    text = input("Enter text: ")
-    createNoteOnDB({"title":title, "text":text}) 
+app = FastAPI()
 
-def displayAllNotes():
-    for i in getAllNotesOnDB():
-        print(i)
 
-def displayMenu():
-    print("\n\tMenu\n1 : Display all notes\n2 : Create note\n3 : Delete note by ID\n4 : Search for note by title\n0 : Exit")
-
-def deleteNoteById():
-    id = input("Enter ID: ")
-    deleteByIdOnDB(id)
-
-def searchByTitle():
-    title = input("Enter note title:")
-    print(searchByTitleOnDB(title))
-    
-
-running = True
-while running:
-    displayMenu()
-    num = input("Enter: ")
-    if num == "1":
-        displayAllNotes()
-    elif num == "2":
-        createNote()
-        displayAllNotes()
-    elif num == "3":
-         deleteNoteById()
-         displayAllNotes()
-    elif num == "4":
-         searchByTitle()
-    elif num == "0":
-        running = False
-
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
