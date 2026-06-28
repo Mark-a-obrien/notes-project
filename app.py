c17c5db088f59b63a10fa2757422f76d90bf6936
Mark <84234570+Mark-a-obrien@users.noreply.github.com>
2026-06-28 17:28:27 +0100

    Updated
    Added api key

--- app.py
+++ app.py
@@ -2,12 +2,18 @@ from fastapi import FastAPI
 from db import *
 from pydantic import BaseModel
 
+from dotenv import load_dotenv
+import os
+
 app = FastAPI()
 
 class Note(BaseModel):
     title: str
     text: str
 
+class ApiCall(BaseModel):
+    api_key: str
+    note: Note
 
 @app.get("/")
 async def root():
@@ -19,7 +25,17 @@ async def getNotes():
     return getAllNotesOnDB()
 
 @app.post("/api/create/note")
-async def create_note(note: Note):
-    print(type(note))
-    createNoteOnDB(note) 
-    return note
+async def create_note(apiCall: ApiCall):
+    if not checkApiKey(apiCall.api_key): return {"message": "Invalid api key"}
+
+    createNoteOnDB(apiCall.note) 
+    return apiCall.note
+    
+
+def checkApiKey(api_key):
+    load_dotenv()
+    saved_api_key = os.getenv('api_key')
+
+    if api_key == saved_api_key:
+        return True
+    return False
