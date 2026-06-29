7a7b7879822cc4afd81efe4f0783c2a0359fc739
Mark <84234570+Mark-a-obrien@users.noreply.github.com>
2026-06-29 15:18:29 +0100

    Updated
    Added get_api_key to check if the user had a valid api key for a get request

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ from pydantic import BaseModel
 from dotenv import load_dotenv
 import os
 
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import APIKeyHeader
+
 app = FastAPI()
+
+api_key_header = APIKeyHeader(name="x-api-key", auto_error=False)
 
 class Note(BaseModel):
     title: str
@@ -15,13 +20,22 @@ class ApiCall(BaseModel):
     api_key: str
     note: Note
 
+def get_api_key(api_key_header: str = Depends(api_key_header)):
+    if checkApiKey(api_key_header):
+        return api_key_header
+    
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid or missing API Key"
+    )
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
 @app.get("/api/notes")
-async def getNotes():
+async def getNotes(api_key: str = Depends(get_api_key)):
     return getAllNotesOnDB()
 
 @app.post("/api/create/note")
